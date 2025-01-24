6d926dbb766db356caa53f6f30a51a5e3a94891a
Bilalgithub009 <m.bilaljaliyawala@gmail.com>
2025-01-24 21:45:39 +0500

    testing

--- index.js
+++ index.js
@@ -3,38 +3,38 @@ import express from 'express';
 import morgan from 'morgan';
 
 
-// const tasks = [
-//     {
-//         id: 1,
-//         title: 'Learn Node.js',
-//         completed: false
-//     },
-//     {
-//         id: 2,
-//         title: 'Learn Express',
-//         completed: false
-//     },
-//     {
-//         id: 3,
-//         title: 'Learn MongoDB',
-//         completed: false
-//     },
-//     {
-//         id: 4,
-//         title: 'Learn React',
-//         completed: false,
-//     },
-//     {
-//         id: 5,
-//         title: 'namaz',
-//         completed: false
-//     },
-//     {
-//         id: 6,
-//         title: 'khana',
-//         completed: false
-//     },
-// ];
+const tasks = [
+    {
+        id: 1,
+        title: 'Learn Node.js',
+        completed: false
+    },
+    {
+        id: 2,
+        title: 'Learn Express',
+        completed: false
+    },
+    {
+        id: 3,
+        title: 'Learn MongoDB',
+        completed: false
+    },
+    {
+        id: 4,
+        title: 'Learn React',
+        completed: false,
+    },
+    {
+        id: 5,
+        title: 'namaz',
+        completed: false
+    },
+    {
+        id: 6,
+        title: 'khana',
+        completed: false
+    },
+];
 
 
 
@@ -46,8 +46,8 @@ app.use(morgan('tiny'));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('server is running');
-    // res.status(200).send(tasks);
+    // res.send('server is running');
+    res.status(200).send(tasks);
     
 });
 
